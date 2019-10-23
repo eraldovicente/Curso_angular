6153510c8a91b8895f058e9f46f055605b160808
Eraldo Vicente <eraldovicente2@gmail.com>
2019-10-23 14:00:36 -0300

    Curso Angular

---
+++ communication-app/src/app/event/child-item/child-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child-item',
+  templateUrl: './child-item.component.html',
+  styleUrls: ['./child-item.component.css']
+})
+export class ChildItemComponent implements OnInit {
+
+  @Input() title: string;
+  @Output() inc = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  btnClick(n) {
+    this.inc.emit(n);
+  }
+}
---
+++ communication-app/src/app/parent-child/parent-child.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
+
+@Component({
+  selector: 'app-parent-child',
+  templateUrl: './parent-child.component.html',
+  styleUrls: ['./parent-child.component.css']
+})
+export class ParentChildComponent implements OnInit {
+
+  @ViewChild("stopwatch2", {static: false})
+  private mytimer: TimerComponent;
+
+  @ViewChild("myP", {static: false})
+  private myp: ElementRef;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  start() {
+    this.mytimer.start();
+  }
+
+  stop() {
+    this.mytimer.stop();
+  }
+
+  clear() {
+    this.mytimer.clear();
+  }
+
+  ngAfterViewInit() {
+    console.log(this.myp);
+  }
+
+}
